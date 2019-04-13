fc0ccb10ccffd915ad4cacd67f7d9972c3b3e2df
linhngoc <linhhr223@gmail.com>
2019-04-13 22:47:07 +0700

    first commit

---
+++ Components/header.php
@@ -0,0 +1,12 @@
+<html>
+<header>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="../Asset/css/style.css">
+</header>
+<body>
+<?php
+require "../Database/config.php";
+
+?>
+
+
---
+++ index.php
@@ -0,0 +1,82 @@
+<html>
+<header>
+    <title>HOME PAGE</title>
+    <link rel="stylesheet" href="Asset/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</header>
+<body>
+    <h1>HOME PAGE</h1>
+
+    <h5>PHOTOGRAPHY</h5>
+
+    <div class="menu">
+        <ul>
+            <li><a href="index.php">Home Page</a></li>
+            <li><a href="#" id="menu">Category</a></li>
+            <li><a href="Login/SignIn.php">Sign In</a></li>
+            <li><a href="Login/SignUp.php">Sign Up</a></li>
+            <li>
+                <input type="text" name="search" value="search">
+            </li>
+        </ul>
+    </div>
+
+    <div class="content">
+        <br><br><br><br>
+        <hr style="width:95%; margin: auto;">
+        <div class="category">Nature</div>
+
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+
+        <br>
+        <a class="link" href="#">Show more ...</a>
+    </div>
+
+    <div class="content">
+        <br><br><br><br>
+        <hr style="width:95%; margin: auto;">
+        <div class="category">Animal</div>
+
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+
+        <br>
+        <a class="link" href="#">Show more ...</a>
+    </div>
+
+    <div class="content">
+        <br><br><br><br>
+        <hr style="width:95%; margin: auto;">
+        <div class="category">Human</div>
+
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+        <a href="#"><img src="Asset/image/bg.png" alt="img1"/></a>
+
+        <br>
+        <a class="link" href="#">Show more ...</a>
+    </div>
+
+    <div class="footer">
+        <ul>
+            <li><i class="fab fa-facebook"></i></li>
+            <li><i class="fab fa-twitter"></i></li>
+            <li><i class="fab fa-instagram"></i></li>
+        </ul>
+
+        <p>Front end made by Linh 3C 22:29 13-4-2019</p>
+
+    </div>
+</body>
+</html>
